ee46aedc62ae3f1a7dd73f422446f84b16db409a
Alvaro Ribeiro Pereira <alvaro.r.p@hotmail.com>
2023-05-16 15:15:21 -0300

    Integrado ao Swagger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json";
 const app = express();
 app.use(express.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // não dinâmico por conta da config do Nginx
 const port = 3000;
@@ -9,9 +12,8 @@ import homeRota from "./Routes/home";
 import Lancamento from "./Routes/Lancamento";
 import { JWTDecode } from "./Middleware/JWTDecode";
 
-
 app.use("/", homeRota);
-app.use("/lancamento", JWTDecode('Caixa'), Lancamento);
+app.use("/lancamento", JWTDecode("Caixa"), Lancamento);
 
 app.listen(port, () => {
   console.log(`🙌😎 Servidor HTTP rodando porta: ${port} 👌`);
